396aa620c562a75caf649f4bc9a091777b91dc7a
littleBao <zhongw>
2016-04-04 09:31:28 +0800

    add ui-Router

---
+++ uiRouter/js/app.js
@@ -0,0 +1,21 @@
+var myApp = angular.module("myApp", ['ui.router']);
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when("", "/pagetab");
+    $stateProvider
+        .state("pagetab", {
+            url:"/pagetab",
+            templateUrl:"pagetab.html"
+        })
+        .state("pagetab.page1", {
+            url:"/page1",
+            templateUrl:"page1.html"
+        })
+        .state("pagetab.page2", {
+            url:"/page2",
+            templateUrl:"page2.html"
+        })
+        .state("pagetab.page3", {
+            url:"/page3",
+            templateUrl:"page3.html"
+        })
+});
